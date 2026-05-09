b139437eaa0085e4946771e0d9d5bc8f97fc203b
Fariyamehek <fariyamehak153@gmail.com>
2026-05-09 15:43:57 +0530

    lucky digit

---
+++ lucky_digit2.py
@@ -0,0 +1,12 @@
+input_number = int(input("enetr a number to find your lucky digit: "))
+print(f'Number you input is {input_number}')
+
+sum_of_digits = 0
+while input_number != 0:
+    remainder = input_number % 10
+    input_number = input_number // 10
+    sum_of_digits += remainder
+    if sum_of_digits > 9 and input_number == 0:
+        input_number = sum_of_digits
+        sum_of_digits = 0
+print('your lucky digit is', sum_of_digits)
