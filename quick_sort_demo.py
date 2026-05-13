a92cd3cdfac5f2b4d05d1737ad1089b0c37885f5
Fariyamehek <fariyamehak153@gmail.com>
2026-05-13 17:24:01 +0530

    sorting techniques

--- quick_sort_demo.py
+++ quick_sort_demo.py
@@ -1,6 +1,9 @@
 import quick_sort as qs
 
 import sys 
-numbers = []
+numbers = [1, 50, 23, 25, 52, 19, 91, 7, 15, 43, 71]
+print('Numbers before Sort: \n', numbers)
+qs.quick_sort(numbers, 0, len(numbers)-1)
+print('Numbers after Sort: \n', numbers)
   
-numbers = [int(value) for value in sys.argv]
+
